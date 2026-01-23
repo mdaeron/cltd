65be21ccd9b28db5f66f8c1a603a3c3fbd3d78ad
mdaeron <mathieu@daeron.fr>
2026-01-23 17:48:04 +0100

    Add more commands

--- src/cltd/__init__.py
+++ src/cltd/__init__.py
@@ -1,10 +1,10 @@
 import os
+import re
 import subprocess
 
 from sys import argv, platform
 from pathlib import Path
 from shutil import copy2
-from datetime import datetime
 from types import SimpleNamespace
 from colorama import Fore, Style, init
 
@@ -31,20 +31,25 @@ class ToDoList():
 		)
 		self.items = {k+1: i for k,i in enumerate(sorted_items)}
 
-	def flush(self):
+	def flush(self, toflush = 'x'):
 		items = self.items.values()
-		sorted_items = (
-			[i for i in items if i.startswith('+')] +
-			[i for i in items if i.startswith('-')]
-		)
-		self.items = {k+1: i for k,i in enumerate(sorted_items)}
+		items = [i for i in items if i[0] not in toflush]
+		self.items = {k+1: i for k,i in enumerate(items)}
+
+	def print(self):
+		s = ['']
+		l = len(str(len(self.items)))
+		for k,i in self.items.items():
+			i = decode(i)
+			s.append(f"  {k:>{l}.0f}. {i.txt}")
+		print('\n'.join(s))
 
 	def __str__(self):
 		s = ['']
 		l = len(str(len(self.items)))
 		for k,i in self.items.items():
 			i = decode(i)
-			fstr = f" {k:>{l}.0f}. {i.txt}"
+			fstr = f"  {k:>{l}.0f}. {i.txt}"
 			if i.status == '-':
 				fstr = Style.DIM + fstr + Style.RESET_ALL
 			elif i.status == 'x':
@@ -56,50 +61,118 @@ class ToDoList():
 
 def load_todofile():
 	todofile.touch(exist_ok = True)
-	timestamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-	newfile = todofile.with_name(f"{todofile.stem}_{timestamp}{todofile.suffix}")
-	copy2(todofile, newfile)
 	with open(todofile) as fid:
 		todos = fid.readlines()
 	return [i.strip() for i in todos]
 
-def save_todofile(tdl):
+def save_todofile(tdl, Nb = 100):
+	filenames = [
+		todofile.with_name(f"{todofile.stem}-backup-{Nb-N}{todofile.suffix}")
+		for N in range(Nb)
+	] + [todofile]
+	for k in range(Nb):
+		try:
+			copy2(filenames[k+1], filenames[k])
+		except FileNotFoundError:
+			pass
 	with open(todofile, 'w') as fid:
 		fid.write('\n'.join(tdl.items.values()))
 
 def main() -> None:
 
 	if len(argv) < 2:
+
 		tdl = ToDoList(load_todofile())
 		print(tdl)
-	elif argv[1] in '-+x':
-		tdl = ToDoList(load_todofile())
-		tdl.items[len(tdl.items)+1] = ' '.join(argv[1:])
-		tdl.sort()
-		print(tdl)
-		save_todofile(tdl)
-	elif argv[1] == 'flush':
-		tdl = ToDoList(load_todofile())
-		tdl.flush()
-		print(tdl)
-		save_todofile(tdl)
-	elif argv[1] == 'open':
-		if platform == 'win32':
-			os.startfile(todofile)
-		elif platform == 'darwin':
-			subprocess.run(['open', todofile])
-		else:
-			subprocess.run(['xdg-open', todofile])
+
 	else:
-		tdl = ToDoList(load_todofile())
-		if (
-			(s := argv[1][-1]) in '-+x'
-			and
-			(k := int(argv[1][:-1])) in tdl.items
-		):
-			tdl.items[k] = s + tdl.items[k][1:]
+
+		if argv[1] == 'add':
+			if len(argv) < 3:
+				print('Missing task!')
+				exit()
+			txt = ' '.join(argv[2:])
+			if re.compile(r'[-+x]').match(txt):
+				s, txt = txt[0], txt[1:].strip()
+			else:
+				s = '-'
+			tdl = ToDoList(load_todofile())
+			tdl.items[len(tdl.items)+1] = f'{s} {txt}'
 			tdl.sort()
 			print(tdl)
 			save_todofile(tdl)
+
+		elif argv[1] in ('today', 'now', 'top'):
+			tdl = ToDoList(load_todofile())
+			tdl.flush('-x')
+			tdl.print()
+
+		elif argv[1] == 'flush':
+			tdl = ToDoList(load_todofile())
+			tdl.flush()
+			print(tdl)
+			save_todofile(tdl)
+
+		elif argv[1] == 'open':
+			if platform == 'win32':
+				os.startfile(todofile)
+			elif platform == 'darwin':
+				subprocess.run(['open', todofile])
+			else:
+				subprocess.run(['xdg-open', todofile])
+
+		elif argv[1] == 'vi':
+			if platform == 'win32':
+				print('Not sure how to call vi on Windows!')
+			else:
+				subprocess.run(['vi', todofile])
+				tdl = ToDoList(load_todofile())
+				print(tdl)
+
+
+		elif (( # change status of a given task
+			p := re.compile(r'(?P<i>\d*)(?P<s>[-+x])$').match(argv[1])
+		) or (
+			p := re.compile(r'(?P<s>[-+x])(?P<i>\d*)$').match(argv[1])
+		)):
+			i = int(p.group('i'))
+			s = p.group('s')
+			tdl = ToDoList(load_todofile())
+			if i not in tdl.items:
+				print('No task with that index!')
+			else:
+				tdl.items[i] = s + tdl.items[i][1:]
+				tdl.sort()
+				print(tdl)
+				save_todofile(tdl)
+
+		elif ( # if first arg is just numbers
+			re.compile(r'\d*$').match(argv[1])
+		):
+			i = int(argv[1])
+			tdl = ToDoList(load_todofile())
+			if i not in tdl.items:
+				print('No task with that index!')
+			else:
+				if len(argv) < 3:
+					print(f'Missing instruction for task {i}!')
+					exit()
+				if argv[2] in ('x', 'done'):
+					tdl.items[i] = 'x' + tdl.items[i][1:]
+					tdl.sort()
+					print(tdl)
+					save_todofile(tdl)
+				if argv[2] in ('+', 'today', 'now', 'top'):
+					tdl.items[i] = '+' + tdl.items[i][1:]
+					tdl.sort()
+					print(tdl)
+					save_todofile(tdl)
+				if argv[2] in ('-', 'later'):
+					tdl.items[i] = '-' + tdl.items[i][1:]
+					tdl.sort()
+					print(tdl)
+					save_todofile(tdl)
+
 		else:
+
 			print('Unknown command!')
